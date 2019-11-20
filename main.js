a6d83267b386436f9ed3ca849a7c6f172dd649a7
jbchinonso <nwekejohnbosco@gmail.com>
2019-11-20 15:03:00 -0800

    added email validation

---
+++ main.js
@@ -0,0 +1,25 @@
+const errorBox = document.querySelector('.msg')
+errorBox.style.color = 'red'
+const myForm = document.getElementById('myform')
+const pattern = /[A-Z0-9.+%$]+@[A-Z0-9.+]+.[A-Z0-9]{2,4}/igm
+
+myForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const name = document.getElementById('name').value
+    const mail = document.getElementById('mail').value
+    const subject = document.getElementById('subject').value
+    const message = document.getElementById('message').value
+    
+    if(!name || !mail || !subject || !message){
+        errorBox.textContent = 'Please Fill the Empty Fields'
+        setTimeout(()=> errorBox.textContent = '', 3000 );
+    }else if(!pattern.test(mail)){
+        errorBox.textContent = 'Please Provide a Valid Mail'
+        setTimeout(()=> errorBox.textContent = '', 3000 );
+    }else{
+        errorBox.style.color = 'green'
+        errorBox.innerText = 'Your Mail has been sent Successfully'
+    }
+    
+    
+})
