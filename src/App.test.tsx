01bb514c92a7e8e5d78e18b0a203a09bb552390e
Johnbosco <nwekejohnbosco@gmail.com>
2022-05-20 15:50:20 +0100

    add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders My Name on the screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Johnbosco Nweke/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Render button to checkout my blog', async() => {
+  render(<App />);
+  const button = await screen.findAllByRole('button');
+  expect(button).toBeTruthy()
+  expect(button[0]).toHaveTextContent('Checkout my blogs')
+})
---
+++ src/__test__/Navbar.test.tsx
@@ -0,0 +1,60 @@
+import react from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Navbar from '../componenets/Navbar/Navbar'
+
+describe('Test suite for the Navbar', () => {
+    
+    test('logo is in page', () => {
+        render(
+          <Router>
+            <Navbar />
+          </Router>
+        );
+        const logo = screen.getByAltText("JB logo");
+        expect(logo).toBeInTheDocument()
+    });
+
+
+    test('hamburger is in Page', () => {
+        render(
+          <Router>
+            <Navbar />
+          </Router>
+        );
+        const hamburger = screen.getByTestId("hamburger");
+         expect(hamburger).toHaveClass("hamburger");
+    })
+
+    test('nav is in page', async () => {
+        render(
+          <Router>
+            <Navbar />
+          </Router>
+        );
+
+        const nav = screen.getByTestId("nav");
+        expect(nav).toBeDefined();
+       
+    })
+
+    test("link is in page", async () => {
+      render(
+        <Router>
+          <Navbar />
+        </Router>
+      );
+
+     
+      const navlink = await screen.findAllByRole("link");
+        expect(navlink).toHaveLength(5);
+        expect(navlink[0]).toHaveAttribute('href', "/")
+        expect(navlink[0]).toHaveTextContent("About");
+        expect(navlink[1]).toHaveTextContent("Experience");
+        expect(navlink[2]).toHaveTextContent("Work");
+        expect(navlink[3]).toHaveTextContent("Contact");
+        expect(navlink[4]).toHaveTextContent("Resume");
+    
+    });
+})
