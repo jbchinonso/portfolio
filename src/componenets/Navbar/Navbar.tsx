8c9663445233bf3836068f26be5ed6ef1e880557
--global <nwekejohnbosco@gmail.com>
2022-05-30 23:51:26 +0100

    remove unused useState

--- src/componenets/Navbar/Navbar.tsx
+++ src/componenets/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import {useHamburgerContext} from '../../context/hamburgerContext'
 import JB from '../../images/JB.svg';
